f3c1c8db202463d7fafa94c0032ce8b171910332
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-09-01 08:04:16 +0200

    Updates

---
+++ _next/static/chunks/0h1d491d0oais.js
@@ -0,0 +1,11 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/0y-2t38ohvynw.js",
+  "static/chunks/0cyhdn8052e8z.js",
+  "static/chunks/2mrugj30muqn_.js",
+  "static/chunks/0ryv708s-es5d.js",
+  "static/chunks/1b12frd43mdr2.js",
+  "static/chunks/0ye5jjqe1vyc0.css",
+  "static/chunks/3wn8jdn1rmuqj.css",
+  "static/chunks/0uj528npmcmvi.css",
+  "static/chunks/turbopack-1m4sil0x96afu.js"
+])
---
+++ _next/static/twqUZQVt7TT6UoV6mtCS8/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/36irvlzl60uly.js"
+  ],
+  "/_error": [
+    "static/chunks/29ykwkq0udp0e.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
